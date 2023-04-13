66ba3fec7589d88aeced7a44428cf70b42c57f5d
pooranjoyb <pooranjoyb2016@gmail.com>
2023-04-13 11:37:42 +0530

    Defined functions for QR

--- QR_attendance_tracker/generate.py
+++ QR_attendance_tracker/generate.py
@@ -1,34 +1,40 @@
 import qrcode
 import cv2
 
-# Read the student names from the file
-with open('assets/student_data.txt', 'r') as f:
+# Read the student names from the file/database
+with open('static/assets/student_data.txt', 'r') as f:
     names = f.readlines()
 
-print(names)
+def generateQR():
 
-# Generate QR codes for each name
-for name in names:
-    qr = qrcode.make(name)
-    print(f'Generated QR code for {name.strip()}')
-    qr.save('assets/QRs/img.png')
+    names = [item.strip() for item in names]
+    print(names)
+
+    # Generate QR codes for each name
+    for name in names:
+        qr = qrcode.make(name)
+        print(f'Generated QR code for {name}')
+        qr.save(f'static/assets/QRs/{name}.png')
 
 
-# Decoding the QR
-# Name of the QR Code Image file
-filename = "assets/QRs/img.png"
+def decodeQR():
+    # Decoding the QR
+    for name in names:
 
-# read the QRCODE image
-image = cv2.imread(filename)
+        # Name of the QR Code Image file
+        filename = (f"static/assets/QRs/{name}.png")
 
-# initialize the cv2 QRCode detector
-detector = cv2.QRCodeDetector()
+        # read the QRCODE image
+        image = cv2.imread(filename)
 
-# detect and decode
-data, vertices_array, binary_qrcode = detector.detectAndDecode(image)
+        # initialize the cv2 QRCode detector
+        detector = cv2.QRCodeDetector()
 
-# if there is a QR code
-# print the data
-if vertices_array is not None:
-    print("QRCode data:")
-    print(data)
+        # detect and decode
+        data, vertices_array, binary_qrcode = detector.detectAndDecode(image)
+
+        # if there is a QR code
+        # print the data
+        if vertices_array is not None:
+            print("QRCode data:")
+            print(data)
