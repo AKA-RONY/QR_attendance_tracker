233ee25260413f47e038dfaa8a1cfbe1e7be6fcf
pooranjoyb <pooranjoyb2016@gmail.com>
2023-04-13 11:36:49 +0530

    Added sample db

---
+++ QR_attendance_tracker/db.py
@@ -0,0 +1,8 @@
+# Student data will be fetched from the Database
+
+def matchCreds(username, password):
+    if username == "123" and password == "admin":
+        return True
+    else:
+        return False
+
